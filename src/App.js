fffc754a91dbeee71164c0b1c70830030797754e
Andrew Blair <a.a.g.blair@gmail.com>
2023-06-26 12:44:41 +0100

    taken off surge hosting

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Tabs />   
+      <span id="siteseal"><script async type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=tjEsqeCJhruFdb5Tyg5vpMIaCouvvHYgimpECZiwbC2ADmEzTRQbCpe09IXd"></script></span>
     </div>
   );
 }
