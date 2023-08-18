3bf0fd2f35a6c7ee0ce52182bd915ca8549adb93
Andrew Blair <a.a.g.blair@gmail.com>
2023-08-18 10:26:14 +0100

    updated summary and gradient css

--- src/Components/AllTabs/FirstTab.js
+++ src/Components/AllTabs/FirstTab.js
@@ -9,11 +9,14 @@ const FirstTab = () => {
 
   return (
     <section className="FirstTab">
-      <h3>Projects</h3>
+      <summary>
+      <p>I retrained as a software developer in 2023, applying for an intensive 16 week development course at CodeClan.</p>
+      <p>I have produced full stack applications using Python, JavaScript and Java, and am working on more in my spare time.</p>
+      <p>My preference is for front end React coding, CSS, accessibility and design (but I'm happy working anywhere across the stack).</p>
+      <p> You can see a list of my projects - from the coursework and from my spare time - below.</p>
+      </summary>
       <br />
       <aside>Click on project heading to see/hide further info.</aside>
-      <br />
-      <BarryChips />
       <br />
       <ExoWeather />
       <br />
