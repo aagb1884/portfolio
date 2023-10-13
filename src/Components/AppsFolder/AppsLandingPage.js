9f05cf50fc2dbc4be3521271636c4a7e8edb7f6e
Andrew Blair <a.a.g.blair@gmail.com>
2023-10-13 16:01:11 +0100

    landing page nearly done, some css and responsivity needing added

--- src/Components/AppsFolder/AppsLandingPage.js
+++ src/Components/AppsFolder/AppsLandingPage.js
@@ -5,7 +5,23 @@ import Footer from "./Footer";
 const AppsLandingPage = () => {
     return ( 
         <div className="app-landing-page">
-            <h1>DAFT WEE APPS</h1>
+            <div class="bouncing-text">
+                <div class="d">D</div>
+                <div class="a">A</div>
+                <div class="f">F</div>
+                <div class="t">T</div>
+                <div class="space"></div>
+                <div class="w">W</div>
+                <div class="e">E</div>
+                <div class="e">E</div>
+                <div class="space"></div>
+                <div class="a2">A</div>
+                <div class="p">P</div>
+                <div class="p">P</div>
+                <div class="s">S</div>
+                <div class="shadow"></div>
+                <div class="shadow-two"></div>
+            </div>
 
             <section className="app-buttons-component">
 
@@ -40,9 +56,16 @@ const AppsLandingPage = () => {
                         className="button-image"/>
                 <div className="button-text">Big Finish Generator</div>
             </NavLink>
-            </div>
 
+            
+            </div>
             </section>
+
+            <aside className="design-credit">Bouncing Text animation by <a href="https://lenadesign.org/2021/05/18/css-bouncing-text-animation/">Lena Design</a></aside>
+            <p>Three React apps; some requiring esoteric knowledge of <i>Doctor Who</i>, others less so.</p>
+            <p>You can find more information on each on my <a href="https://github.com/aagb1884">GitHub page</a> (there are individual repos for each project, and a repo for this website which includes updated version).</p>
+            <br />
+            <aside>NB.Wesley Snopes is probably not suitable for work.</aside>
         <Footer />    
         </div>
      );
