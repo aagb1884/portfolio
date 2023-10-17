61ed01c7747e39e5567bc334d9a546be29856348
Andrew Blair <a.a.g.blair@gmail.com>
2023-10-17 14:16:39 +0100

    basic github single page app update added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       
-      <Router>
+      <Router basename='portfolio'>
         <Routes>
           <Route path="/" element={<Tabs /> } />
           <Route path="/apps" element={< AppsLandingPage />} />
