4186584bb1bffeffcfe4bf1006d961f63831ea53
Andrew Blair <a.a.g.blair@gmail.com>
2024-04-22 12:47:24 +0100

    adding files again

---
+++ src/Components/About Me.js
@@ -0,0 +1,13 @@
+const AboutMe = () => {
+   
+
+    return ( 
+
+            <summary className='About-Me'>
+            I am a junior software developer, writer and performer based in Edinburgh.
+                </summary>
+            )}
+
+
+ 
+export default AboutMe;
---
+++ src/Components/AppsFolder/EurovisionBingo/Components/Home.js
@@ -0,0 +1,21 @@
+import BingoCard from "./BingoCard";
+
+const HomePage = () => {
+    return ( 
+      <div className="eurovision-bingo-landing">
+        
+        
+          <header className="animate-character">
+            <div className="animated-header">EUROVISION BINGO</div>
+          </header>
+
+            <main>
+              <BingoCard />
+            </main>
+        
+  
+      </div>
+     );
+}
+ 
+export default HomePage;
