902cd6bcbe05297ed4193549e73e684ad5f9af68
Andrew Blair <a.a.g.blair@gmail.com>
2024-04-22 12:53:56 +0100

    Updates

--- src/Components/AppsFolder/BigFinishGenerator/components/ClearAll.js
+++
@@ -1,8 +0,0 @@
-const ClearAll = ({clearAll}) => {
-
-    return ( 
-<button className="clear-all-button" data-testid="clear-all-button" onClick={clearAll}>Clear All</button>
-     );
-}
- 
-export default ClearAll;
